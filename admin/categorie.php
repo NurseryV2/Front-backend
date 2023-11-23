7cc1f798267ec1968a21fc603a959cfbe6d1f3d5
Asmae Hariti <haritiasmaa74@gmail.com>
2023-11-23 23:27:56 +0100

    adding a category

--- admin/categorie.php
+++ admin/categorie.php
@@ -12,20 +12,38 @@
 
 <body class="bg-[#ECECF8]">
     <?php
-   
-   include("./navbar.php");
-   include("./sidebar.php");
-   include("db.php");
-?>
+    include("./navbar.php");
+    include("./sidebar.php");
+    include("db.php");
+
+    // Handle form submission for adding a new category
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add-category"])) {
+        // Get the category name from the form
+        $categoryName = mysqli_real_escape_string($conn, $_POST["category-name"]);
+
+        // Insert the new category into the database
+        $insertQuery = "INSERT INTO categories (name) VALUES ('$categoryName')";
+        $insertResult = mysqli_query($conn, $insertQuery);
+
+        // Check if the insertion was successful
+        if ($insertResult) {
+            echo '<script>alert("Category added successfully!");</script>';
+        } else {
+            echo '<script>alert("Error adding category: ' . mysqli_error($conn) . '");</script>';
+        }
+    }
+
+    ?>
     <div class="p-5 mt-14 sm:ml-64">
-        <div class="relative overflow-x-auto  sm:rounded-lg">
+        <div class="relative overflow-x-auto sm:rounded-lg">
             <div
                 class="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 dark:bg-gray-900">
                 <div>
-                    <button
+                    <!-- Use JavaScript to toggle the visibility of the form -->
+                    <button onclick="toggleFormVisibility()"
                         class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                         type="button">
-                        Add Categorie
+                        Add Category
                     </button>
                 </div>
                 <label for="table-search" class="sr-only">Search</label>
@@ -34,7 +52,7 @@
                     <div class=" w-full">
                         <input type="text" id="simple-search"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search for a categorie" required>
+                            placeholder="Search for a category" required>
                     </div>
                     <button type="submit"
                         class="p-2.5 ms-2 text-sm font-medium text-white bg-[#2F329F] rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -48,53 +66,75 @@
 
             </div>
             <?php
-// Fetch data from the database, including the count of plants for each category
-$query = "SELECT categories.id, categories.name, COUNT(plants.id) AS plant_count
-          FROM categories
-          LEFT JOIN plants ON categories.id = plants.category_id
-          GROUP BY categories.id, categories.name";
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) > 0) {
-    echo '<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Category ID
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Category Name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Number of Plants
-                    </th>
-                </tr>
-            </thead>
-            <tbody>';
+            // Fetch data from the database, including the count of plants for each category
+            $query = "SELECT categories.id, categories.name, COUNT(plants.id) AS plant_count
+                      FROM categories
+                      LEFT JOIN plants ON categories.id = plants.category_id
+                      GROUP BY categories.id, categories.name";
+            $result = mysqli_query($conn, $query);
+            if (mysqli_num_rows($result) > 0) {
+                echo '<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Category ID
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Category Name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Number of Plants
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>';
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                    ' . $row['id'] . '
-                </td>
-                <td class="px-6 py-4">
-                    ' . $row['name'] . '
-                </td>
-                <td class="px-6 py-4">
-                    ' . $row['plant_count'] . '
-                </td>
-            </tr>';
-    }
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                ' . $row['id'] . '
+                            </td>
+                            <td class="px-6 py-4">
+                                ' . $row['name'] . '
+                            </td>
+                            <td class="px-6 py-4">
+                                ' . $row['plant_count'] . '
+                            </td>
+                        </tr>';
+                }
 
-    echo '</tbody></table>';
-} else {
-    echo '<p>No data found</p>';
-}
+                echo '</tbody></table>';
+            } else {
+                echo '<p>No data found</p>';
+            }
 
-mysqli_close($conn);
-?>
+            mysqli_close($conn);
+            ?>
+            <!-- Form for adding a new category (initially hidden) -->
+            <div id="addCategoryForm" class="mt-4 hidden">
+                <h2 class="text-xl font-semibold mb-2">Add New Category</h2>
+                <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                    <label for="category-name" class="sr-only">Category Name</label>
+                    <input type="text" id="category-name" name="category-name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Enter category name" required>
+                    <button type="submit"
+                        class="mt-2 p-2.5 text-sm font-medium text-white bg-[#2F329F] rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        name="add-category">
+                        Add Category
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
-    </div>
+
+    <script>
+    function toggleFormVisibility() {
+        var addCategoryForm = document.getElementById('addCategoryForm');
+        addCategoryForm.style.display = (addCategoryForm.style.display == 'none' || addCategoryForm.style.display ==
+            '') ? 'block' : 'none';
+    }
+    </script>
 </body>
 
 </html>
