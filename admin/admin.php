749e1b1df0eafd879bf3e69f25ca4f8e5e0a898f
Asmae Hariti <haritiasmaa74@gmail.com>
2023-12-03 16:34:30 +0100

    making the adding category as popup

--- admin/admin.php
+++ admin/admin.php
@@ -33,13 +33,36 @@
         <div class="relative overflow-x-auto sm:rounded-lg">
             <div
                 class="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 dark:bg-gray-900">
-                <div>
-                    <button onclick="toggleFormVisibility()"
-                        class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                        type="button">
-                        Add Category
-                    </button>
-                </div>
+
+
+                <button onclick="my_modal_3.showModal()"
+                    class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    type="button">
+                    Add Category
+                </button>
+                <dialog id="my_modal_3" class="modal">
+                    <div class="modal-box">
+                        <form method="dialog">
+                            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        </form>
+                        <div class="mt-4 ">
+                            <h2 class="text-xl font-semibold mb-2">Add New Category</h2>
+                            <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                                <label for="category-name" class="sr-only">Category Name</label>
+                                <input type="text" id="category-name" name="category-name"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Enter category name" required>
+                                <button type="submit"
+                                    class="mt-2 p-2.5 text-sm font-medium text-white bg-[#2F329F] rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    name="add-category">
+                                    Add Category
+                                </button>
+                            </form>
+                        </div>
+
+                    </div>
+                </dialog>
+
                 <label for="table-search" class="sr-only">Search</label>
 
                 <form method="post" action="" id="searchForm" class=" flex items-center">
@@ -62,20 +85,7 @@
             <?php
           
               include("./get_plants.php") ?>
-            <div id="addCategoryForm" class="mt-4 hidden">
-                <h2 class="text-xl font-semibold mb-2">Add New Category</h2>
-                <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-                    <label for="category-name" class="sr-only">Category Name</label>
-                    <input type="text" id="category-name" name="category-name"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Enter category name" required>
-                    <button type="submit"
-                        class="mt-2 p-2.5 text-sm font-medium text-white bg-[#2F329F] rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        name="add-category">
-                        Add Category
-                    </button>
-                </form>
-            </div>
+
 
         </div>
     </div>
