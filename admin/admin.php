e8f68d7bb791d838ce4baac8659af6cbc45ca360
Asmae Hariti <haritiasmaa74@gmail.com>
2023-12-03 12:14:40 +0100

    switching

--- admin/admin.php
+++ admin/admin.php
@@ -116,7 +116,6 @@
 
             </div>
             <?php
-            // Fetch data from the database, including the count of plants for each category
             $query = "SELECT categories.id, categories.name, COUNT(plants.id) AS plant_count
                       FROM categories
                       LEFT JOIN plants ON categories.id = plants.category_id
@@ -245,70 +244,10 @@
     </div>
 
 
-    <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        const searchInput = document.getElementById("simple-search");
-
-        searchInput.addEventListener("input", function() {
-            const searchTerm = searchInput.value.trim();
-
-            if (searchTerm.length > 0) {
-                // Make an AJAX request to the search_category.php file
-                fetch("search_category.php", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/x-www-form-urlencoded",
-                        },
-                        body: `searchTerm=${encodeURIComponent(searchTerm)}`,
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        // Update the UI with the search results
-                        updateCategoryList(data);
-                    })
-                    .catch(error => {
-                        console.error("Error fetching data:", error);
-                    });
-            }
-        });
-
-        function updateCategoryList(categories) {
-            const categoryListContainer = document.getElementById("category-list");
-            categoryListContainer.innerHTML = "";
-            categories.forEach(category => {
-                const listItem = document.createElement("li");
-                listItem.textContent = category.name;
-                categoryListContainer.appendChild(listItem);
-            });
-        }
-    });
-
-
-
-
-    function openModal(modalId) {
-        var modal = document.getElementById(modalId);
-        if (modal) {
-            modal.showModal();
-        }
-    }
-
-    function toggleFormVisibility() {
-        var addCategoryForm = document.getElementById('addCategoryForm');
-        addCategoryForm.style.display = (addCategoryForm.style.display == 'none' || addCategoryForm.style.display ==
-            '') ? 'block' : 'none';
-    }
-
-    function toggleFormsVisibility() {
-        var addCategoryForm = document.getElementById('updateCategory');
-        addCategoryForm.style.display = (addCategoryForm.style.display == 'none' || addCategoryForm.style.display ==
-            '') ? 'block' : 'none';
-    }
-    </script>
 
 </body>
 
 
-
+<script src="../js/script.js"></script>
 
 </html>
